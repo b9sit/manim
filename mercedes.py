0af11cc3000184aee60063da6a1d79d0b824b8af
k <183429359+b9sit@users.noreply.github.com>
2025-05-19 22:24:56 +0100

    Implemented camera tracking the car

--- mercedes.py
+++ mercedes.py
@@ -6,7 +6,20 @@ class MercRev(MovingCameraScene):
     def construct(self):
         rad = 2
         base_angle = np.pi / 2
-        rotation_tracker = ValueTracker(0)
+
+        bg = Rectangle(
+            width=100,
+            height=self.camera.frame_height + 10,
+            stroke_width=0,
+        )
+        bg.set_fill(
+            color=[DARK_BLUE, BLACK],  
+            opacity=1
+        )
+
+        bg.z_index = -10
+
+        self.add(bg)
 
         angles = [base_angle, base_angle + 2 * np.pi / 3, base_angle + 4 * np.pi / 3]
         points = [np.array([rad * np.cos(a), rad * np.sin(a), 0]) for a in angles]
@@ -28,15 +41,30 @@ class MercRev(MovingCameraScene):
         rectangle.next_to(trapezium, DOWN, buff=0)
         rectangle.shift(RIGHT * 0.5)
 
-        tire1 = Circle(radius=0.4)
-        tire1.set_fill(BLACK, opacity=1)        
-        tire1.set_stroke(WHITE, width=3)         
+        def create_tire(radius=0.4, rim_lines=5, rim_length=0.8, rim_color=WHITE):
+            tire = Circle(radius=radius)
+            tire.set_fill(BLACK, opacity=1)
+            tire.set_stroke(WHITE, width=3)
+
+            rims = VGroup()
+            angle_between = TAU / rim_lines
+            for i in range(rim_lines):
+                line = Line(ORIGIN, rim_length * UP, stroke_width=2, color=rim_color)
+                line.rotate(i * angle_between)
+                rims.add(line)
+
+            rims.move_to(tire.get_center()) 
+
+            tire_group = VGroup(tire, rims)
+            return tire_group
+
+
+
+        tire1 = create_tire()
         tire1.next_to(trapezium, DOWN, buff=0.6)
         tire1.shift(LEFT * 1.5)
 
-        tire2 = Circle(radius=0.4, color=WHITE)
-        tire2.set_fill(BLACK, opacity=1)
-        tire2.set_stroke(WHITE, width=3)         
+        tire2 = create_tire()
         tire2.next_to(trapezium, DOWN, buff=0.6)
         tire2.shift(RIGHT * 2)
 
@@ -52,18 +80,40 @@ class MercRev(MovingCameraScene):
         text.next_to(mercLogo, DOWN, buff=0.2)
 
         logo = VGroup(mercLogo, text)
-    
+
+        def follow_target(frame: Mobject):
+            frame.move_to(car.get_center())
+
+        self.camera.frame.add_updater(follow_target)
+        self.add(self.camera.frame)
+
+        def spin_tire(mObj, dt):
+            mObj.rotate(-TAU * dt * 5)  
+
+        tire1.add_updater(spin_tire)
+        tire2.add_updater(spin_tire)
+
         self.play(
-        AnimationGroup(
-        Rotate(mercLogo, angle=TAU*3, run_time=2, rate_func=rf.ease_out_sine),
-        car.animate.move_to([0, -2, 0]),
-        lag_ratio=0.5,
-        run_time=2,
-        rate_func=rf.ease_out_sine
-            )
+            car.animate.move_to([0, -2, 0]),
+            run_time=2,
+            rate_func=rf.ease_in_sine
         )
-        self.play(FadeIn(text, rate_func=rf.ease_out_sine))
+
+        tire1.remove_updater(spin_tire)
+        tire2.remove_updater(spin_tire)
+
+        self.camera.frame.remove_updater(follow_target)
         self.play(
-        self.camera.frame.animate.move_to(logo).scale(0.5), run_time=0.5, rate_func=rf.ease_out_sine)
+            self.camera.frame.animate.move_to(ORIGIN),
+            run_time=1,
+            rate_func=rf.ease_in_out_sine
+        )
+
+        self.play(FadeIn(mercLogo))
+        self.play(Rotate(mercLogo, angle=TAU*3, run_time=1, rate_func=rf.ease_out_sine))
+
+        self.play(
+        self.camera.frame.animate.move_to(logo).scale(0.5), run_time=0.5, rate_func=rf.ease_in_sine)
+        self.play(FadeIn(text, rate_func=rf.ease_out_sine))
 
         self.wait(1)
